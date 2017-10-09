de109dfa2e77ff64cb2f86c64d4b27676954b5c7
eriksheffer <32528518+eriksheffer@users.noreply.github.com>
2017-10-09 19:15:08 -0400

    Quick test of committing from VS, added a constants class and a boolean board using the constant Queen.

--- NQueens/Program.cs
+++ NQueens/Program.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace NQueens
 {
+    public static class Constants
+    {
+        public const int Queen = 9;
+    }
     class Program
     {
         static void Main(string[] args)
         {
             String whatever = "Hello world!!!!!!!!!!";
+            bool[,] board = new bool[Constants.Queen,Constants.Queen];
         }
+        
     }
 }
