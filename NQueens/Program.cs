ca81f2a08010ea42ab0ef2c3b15ec9a99f31125d
eriksheffer <32528518+eriksheffer@users.noreply.github.com>
2017-10-11 19:12:45 -0400

    PrintBoard Function()

--- NQueens/Program.cs
+++ NQueens/Program.cs
@@ -14,6 +14,12 @@ namespace NQueens
     class Program
     {
         public static bool found = false;
+
+        //  FOR THE BOARD:
+        //  2 MEANS QUEEN
+        //  1 MEANS INVALID
+        //  0 MEANS AVAILABLE
+
         static void Main(string[] args)
         {
             //Create a stopwatch for Measuring the total time taken.
@@ -22,16 +28,25 @@ namespace NQueens
 
             
             int[,] board = new int[Constants.Queen,Constants.Queen];
-            board[2, 2] = 99;
             int[,] complete = placeQueen(board,1,1);
             //Stop the watch, and output the amount of Milliseconds taken. Should be after everything is done.
             //System.Threading.Thread.Sleep(5000);            //Forces the App to wait for now to make sure stopwatch is working, will be removed.
             watch.Stop();
             long time = watch.ElapsedMilliseconds;
             Console.WriteLine(time);
+            printBoard(complete);
         }
 
 
+        //**********************************************************************************************
+        /// <summary>
+        /// Places a Queen on the board and marks all of the spaces that it threatens Invalid
+        /// </summary>
+        /// <param name="nboard"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        //**********************************************************************************************
         public static int[,] placeQueen(int[,] nboard, int row, int col)
         {
             //Performs a Deep copy of array
@@ -53,21 +68,33 @@ namespace NQueens
                     if (copyboard[r, c] == 0)
                         copyboard[r, c] = 1;
                 //Marking Upper-right Diagonals
-                for (int r = row, c = col; r >= 0 && c < nboard.Length; r--, c++)
+                for (int r = row, c = col; r >= 0 && c < Constants.Queen; r--, c++)
                     if (copyboard[r, c] == 0)
                         copyboard[r, c] = 1;
                 //Marking Lower-left Diagonals
-                for (int r = row, c = col; r < nboard.Length && c >= 0; r++, c--)
+                for (int r = row, c = col; r < Constants.Queen && c >= 0; r++, c--)
                     if (copyboard[r, c] == 0)
                         copyboard[r, c] = 1;
                 //Marking Lower-right Diagonals
-                for (int r = row, c = col; r < nboard.Length && c < nboard.Length; r++, c++)
+                for (int r = row, c = col; r < Constants.Queen && c < Constants.Queen; r++, c++)
                     if (copyboard[r, c] == 0)
                         copyboard[r, c] = 1;
                 #endregion
             }
 
             return copyboard;
+        }
+
+        public static void printBoard(int[,] board)
+        {
+            for (int row = 0; row < Constants.Queen; row++)
+            {
+                for (int col = 0; col < Constants.Queen; col++)
+                {
+                    Console.Write(board[row, col]+"|");
+                }
+                Console.WriteLine();
+            }
         }
 
 
